5631db951d5ab406f25e82ef09ccd24083d0ba09
Justin Jr <minh-thong.lu@cyberskill.tech>
2020-04-14 00:49:41 +0700

    fix/fix-production-bug-private-key

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 import mongoose from './utils/mongoose'; // Setup MongoDB Database
-// import agenda from './utils/agenda';
-// import dailyMentorKpiJobs from './cronjobs/daily-mentor-kpi';
-// import dailyStudentKpiJobs from './cronjobs/daily-student-kpi';
+import agenda from './utils/agenda';
+import dailyMentorKpiJobs from './cronjobs/daily-mentor-kpi';
+import dailyStudentKpiJobs from './cronjobs/daily-student-kpi';
 
 function onDatabaseConnected() {
-  // agenda.start();
-  // dailyMentorKpiJobs();
-  // dailyStudentKpiJobs();
+  agenda.start();
+  dailyMentorKpiJobs();
+  dailyStudentKpiJobs();
 }
 
 mongoose.connect(onDatabaseConnected);
